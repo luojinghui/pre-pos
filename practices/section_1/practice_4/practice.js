2df7414e613f4cca090258d128935992c6d012b6
luojinghui <939035243@qq.com>
2015-05-17 12:20:28 +0800

    add fourth question

--- practices/section_1/practice_4/practice.js
+++ practices/section_1/practice_4/practice.js
@@ -1,5 +1,18 @@
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
+
+  var  collection_same= [];
+for (var  i = 0;  i < collection_a.length; i ++){
+    for(var j = 0; j < object_b.value.length; j++){
+           if(collection_a[i].key ==object_b.value[j]){
+                 collection_same.push(object_b.value[j]);
+           }
+    }
+}
+       return collection_same;
+
+
+
 }
 
 module.exports = collect_same_elements;
