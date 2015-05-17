2b16d0b8764debf22168ffecac652b5c42b97f08
luojinghui <939035243@qq.com>
2015-05-17 11:33:29 +0800

    add the second question

--- practices/section_1/practice_2/practice.js
+++ practices/section_1/practice_2/practice.js
@@ -1,5 +1,18 @@
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
+  var  common= [];
+for (var  i = 0;  i < collection_a.length; i ++){
+    for(var j = 0; j < collection_b[0].length; j++){
+           if(collection_a [i] ==collection_b[0][j]){
+                 common.push(collection_a[i]);
+           }
+    }
+}
+       return common;
+
+
+
+
 }
 
 module.exports = collect_same_elements;
--- practices/section_2/practice_1/practice.js
+++ practices/section_2/practice_1/practice.js
@@ -1,5 +1,13 @@
-function count_same_elements(collection) {
-  //在这里写入代码
+function collect_same_elements(collection_a, collection_b) {
+ //在这里写入代码
+ var  common= [];
+for (var  i = 0;  i < collection_a.length; i ++){
+   for(var j = 0; j < 4; j++){
+          if(collection_a [i] ==collection_b[0][j]){
+                common.push(collection_a[i]);
+          }
+   }
 }
-
-module.exports = count_same_elements;
+      return common;
+}
+module.exports = collect_same_elements;
