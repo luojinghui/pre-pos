272b9e102b59ad00c010019a55d60ab5ac2aa683
luojinghui <939035243@qq.com>
2015-05-18 16:30:03 +0800

    add tenth question own

--- practices/section_3/practice_3/practice.js
+++ practices/section_3/practice_3/practice.js
@@ -1,28 +1,32 @@
 function create_updated_collection(collection_a, object_b) {
-  var c=[];
-  var sum={};
-  var choose = true;
-  for(var i = 0;i<collection_a.length;i++){
-      if(choose){
-      sum={key:collection_a[i],count:0};
-      choose = false;
-    }
-    sum.count+=1;
-    if(collection_a[i]!=collection_a[i+1]){
-      c.push(sum);
-      choose=true;
-    }
-  }
-  for(var i=0;i<c.length;i++){
-      for(x=0;x<object_b.value.length;x++){
-          if(c[i].key==object_b.value[x]){
-            if(c[i].count/3>0){
-              c[i].count-=parseInt(c[i].count/3);
+    var create_array = [];
+    for(var i = 0; i < collection_a.length; i ++){
+        var exist = false;
+        var char = collection_a[i];
+        var count = 1;
+        for(var j = 0; j < create_array.length; j ++){
+            if(create_array[j].key==char){
+                create_array[j].count = create_array[j].count + 1;
+                exist = true;
             }
-          }
-      }
-  }
-  return c;
+        }
+        if(!exist){
+            var add = {};
+            add.key = collection_a[i];
+            add.count = count;
+            create_array.push(add);
+        }
+    }
+    for(var x = 0;x < create_array.length ; x ++){
+             for(y = 0;y < object_b.value.length; y ++){
+                 if(create_array[x].key == object_b.value[y]){
+                     if(create_array[x].count/3>0){
+                         create_array[x].count=create_array[x].count-parseInt(create_array[x].count/3);
+                  }
+             }
+         }
+    }
+    return create_array;
 }
 
 module.exports = create_updated_collection;
