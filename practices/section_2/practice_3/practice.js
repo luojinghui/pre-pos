3915424199cfdf0724dc47815632314e823afc1a
luojinghui <939035243@qq.com>
2015-05-18 14:56:29 +0800

    add seventh question answer 2

--- practices/section_2/practice_3/practice.js
+++ practices/section_2/practice_3/practice.js
@@ -25,3 +25,34 @@ function count_same_elements(collection) {
 }
 
 module.exports = count_same_elements;
+
+
+/*方法2，通过截取字符串进行添加；
+function count_same_elements(collection) {
+    var sum_list=[];
+    for(var i=0;i<collection.length;i++) {
+            var summary=1;
+            var name=collection[i].substring(0,1);
+            if(collection[i].indexOf("-")>-1||collection[i].indexOf(":") >-1||collection[i].indexOf("[") >-1) {
+                summary=collection[i].substring(2);
+                summary=parseInt(summary);
+             }
+             var exsit=false;
+             for(var k=0;k<sum_list.length;k++) {
+                 if(sum_list[k].name==name) {
+                          sum_list[k].summary=sum_list[k].summary+summary;
+                          exsit=true;
+                 }
+             }
+             if(!exsit) {
+                    var temp={};
+                    temp.name=name;
+                    temp.summary=summary;
+                    sum_list.push(temp);
+             }
+        }
+        return sum_list;
+}
+
+module.exports = count_same_elements
+*/
